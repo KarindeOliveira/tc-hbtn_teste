e746588807e4797e2c6d100e7f938bf84d8518ac
Karin Pinheiro <karindanoli@gmail.com>
2022-07-19 09:13:23 -0300

    first commit

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersonTest {
 
     Person op = new Person();
@@ -28,25 +29,25 @@ public class PersonTest {
     @ParameterizedTest(name = "testa o metodo check_password()")
     @ValueSource(strings = { "123456789", "#$%1234" })
     void does_not_have_letters(String input){
-        Assertions.assertFalse(op.checkUser(input));
+        Assertions.assertFalse(op.checkPassword(input));
     }
 
     @ParameterizedTest(name = "testa o metodo check_password()")
     @ValueSource(strings = { "Abcabcdefgh@", "#hbtn@%tc"})
     void does_not_have_numbers(String input){
-        Assertions.assertTrue(op.checkUser(input));
+        Assertions.assertFalse(op.checkPassword(input));
     }
 
     @ParameterizedTest(name = "testa o metodo check_password()")
     @ValueSource(strings = { "Abc@123", "12$@hbt" })
     void does_not_have_eight_chars(String input){
-        Assertions.assertFalse(op.checkUser(input));
+        Assertions.assertFalse(op.checkPassword(input));
     }
 
     @ParameterizedTest(name = "testa o metodo check_password()")
     @ValueSource(strings = { "abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123" })
     void check_password_valid(String input){
-        Assertions.assertTrue(op.checkUser(input));
+        Assertions.assertFalse(op.checkPassword(input));
     }
 
     }
